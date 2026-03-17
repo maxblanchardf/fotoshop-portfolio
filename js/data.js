105e8241bf52c1288e21b1a118720b857ee8a8ca
maxblanchardf <max.blanchard@gmail.com>
2026-03-17 20:47:43 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,4 +3,12 @@
 
 const PHOTOS = [
   { id: 41, src: 'photos/lenceria/13273-241_1.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 42, src: 'photos/lenceria/25488-2-1.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 43, src: 'photos/lenceria/9683-1-5.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 44, src: 'photos/lenceria/9683-2-5.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 45, src: 'photos/lenceria/25486-2-1.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 46, src: 'photos/lenceria/90000-94-2.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 47, src: 'photos/lenceria/90003-1-3.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 48, src: 'photos/lenceria/grupo-ana-y-gaby-2.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 49, src: 'photos/lenceria/25511-172-1.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
 ];
