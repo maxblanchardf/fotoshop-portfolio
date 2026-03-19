8e6cf9fbb6f152a42989dff5285140a163d7c8e3
maxblanchardf <max.blanchard@gmail.com>
2026-03-19 16:40:48 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -176,4 +176,6 @@ const PHOTOS = [
   { id: 259, src: 'photos/moda-mujer/pantes-flare-bolsillo-ecru-5.jpg', category: 'moda-mujer', categoryLabel: 'Moda Mujer', client: 'Raindoor', title: '' },
   { id: 260, src: 'photos/moda-mujer/pants-bag-blue-1.jpg', category: 'moda-mujer', categoryLabel: 'Moda Mujer', client: 'Raindoor', title: '' },
   { id: 261, src: 'photos/lenceria/25483-23-3.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 262, src: 'photos/lenceria/5184-135-2.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
+  { id: 263, src: 'photos/lenceria/9480-1-1.jpg', category: 'lenceria', categoryLabel: 'Lencería', client: 'Flores', title: '' },
 ];
